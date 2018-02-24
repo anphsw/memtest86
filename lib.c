aae52908fcded762804894630b6a36e59de74359
root <sysop@880.ru>
2018-02-24 18:07:57 +0700

    fix data-bits parsing in console= option

--- lib.c
+++ lib.c
@@ -1149,7 +1149,7 @@ void serial_console_setup(char *param)
 
 	end++;
 
-	if (*end != '\0' || *end != ' ')
+	if (*end != '\0' && *end != ' ')
 		return;  /* garbage at the end */
 
 	serial_bits = bits;
