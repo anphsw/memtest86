anphsw/memtest86

commit f0560a599053a2f1e9273ffd569c9f868ffa1f86

rewrite popup popdown popclear by Maxime de Roucy <maxime.deroucy@gmail.com>

lib.c
@@ -603,23 +603,30 @@ void set_cache(int val)
 int get_key() {
 	int c;
 
-	c = inb(0x64);
-	if (c & 0x01) {
-		c = inb(0x60);
-		return((c));
-	} else if (serial_cons) {
-		int comstat;
-		comstat = serial_echo_inb(UART_LSR);
-		if (comstat & UART_LSR_DR) {
-			c = serial_echo_inb(UART_RX);
-			/* Pressing '.' has same effect as 'c'
-			   on a keyboard.
-			   Oct 056   Dec 46   Hex 2E   Ascii .
-			*/
-			return (ascii_to_keycode(c));
-		}
-	}
-	return(0);
+/*	c = inb(0x64);
+	itoa(test, c);
+	serial_echo_print("aa");
+	serial_echo_print(test);
+	serial_echo_print("aa");
+
+	if ((c & 1) == 0) {
+		serial_echo_print("bbbbb");*/
+//		if (serial_cons) {
+			int comstat;
+			comstat = serial_echo_inb(UART_LSR);
+			if (comstat & UART_LSR_DR) {
+				c = serial_echo_inb(UART_RX);
+				/* Pressing '.' has same effect as 'c'
+				   on a keyboard.
+				   Oct 056   Dec 46   Hex 2E   Ascii .
+				*/
+				return (ascii_to_keycode(c));
+			}
+//		}
+		return(0);
+/*	}
+	c = inb(0x60);
+	return((c));*/
 }
 
 void check_input(void)
@@ -649,7 +656,9 @@ void check_input(void)
 			break;
 		case 0x26:
 			/* ^L/L - redraw the display */
-			tty_print_screen();
+		        clear_screen_buf();
+    tty_print_region(0, 0, 80,100);
+//			tty_print_screen();
 			break;
 		}
 	}

config.c
@@ -25,8 +25,9 @@ extern short btflag;
 
 extern void get_list(int x, int y, int len, char *buf);
 
-char save[2][POP_H][POP_W];
-char save2[2][POP2_H][POP2_W];
+/* declared in test.h */
+char pop_save_buffer_1[2][POP_H][POP_W];
+char pop_save_buffer_2[2][POP2_H][POP2_W];
 
 void get_config()
 {
@@ -35,7 +36,7 @@ void get_config()
   char cp[64];
 	ulong page;
 
-	popup();
+	popup(POP_SAVE_BUFFER_1);
 	wait_keyup();
 	while(!flag) {
 		cprint(POP_Y+1,  POP_X+2, "Settings:");
@@ -54,7 +55,7 @@ void get_config()
 		switch(get_key()) {
 		case 2:
 			/* 1 - Test Selection */
-			popclear();
+			popclear(POP_SAVE_BUFFER_1);
 			cprint(POP_Y+1, POP_X+2, "Test Selection:");
 			cprint(POP_Y+3, POP_X+6, "(1) Default Tests");
 			cprint(POP_Y+4, POP_X+6, "(2) Skip Current Test");
@@ -86,7 +87,7 @@ void get_config()
 					break;
 				case 4:
 					/* Select test */
-					popclear();
+					popclear(POP_SAVE_BUFFER_1);
 					cprint(POP_Y+1, POP_X+3,
 						"Test Selection:");
 					cprint(POP_Y+4, POP_X+5,
@@ -118,7 +119,7 @@ void get_config()
 					break;
 				case 5:
 					/* Enter a test list */
-					popclear();
+					popclear(POP_SAVE_BUFFER_1);
 					cprint(POP_Y+1, POP_X+3,
 				"Enter a comma separated list");
 					cprint(POP_Y+2, POP_X+3,
@@ -176,11 +177,11 @@ void get_config()
 					break;
 				}
 			}
-			popclear();
+			popclear(POP_SAVE_BUFFER_1);
 			break;
 		case 3:
 			/* 2 - Address Range */
-			popclear();
+			popclear(POP_SAVE_BUFFER_1);
 			cprint(POP_Y+1, POP_X+2, "Test Address Range:");
 			cprint(POP_Y+3, POP_X+6, "(1) Set Lower Limit");
 			cprint(POP_Y+4, POP_X+6, "(2) Set Upper Limit");
@@ -191,7 +192,7 @@ void get_config()
 				switch(get_key()) {
 				case 2:
 					/* Lower Limit */
-					popclear();
+					popclear(POP_SAVE_BUFFER_1);
 					cprint(POP_Y+2, POP_X+4,
 						"Lower Limit: ");
 					cprint(POP_Y+4, POP_X+4,
@@ -212,7 +213,7 @@ void get_config()
 					break;
 				case 3:
 					/* Upper Limit */
-					popclear();
+					popclear(POP_SAVE_BUFFER_1);
 					cprint(POP_Y+2, POP_X+4,
 						"Upper Limit: ");
 					cprint(POP_Y+4, POP_X+4,
@@ -250,11 +251,11 @@ void get_config()
 					break;
 				}
 			}
-			popclear();
+			popclear(POP_SAVE_BUFFER_1);
 			break;
 		case 4:
 			/* Error Mode */
-			popclear();
+			popclear(POP_SAVE_BUFFER_1);
 			cprint(POP_Y+1, POP_X+2, "Printing Mode:");
 			cprint(POP_Y+3, POP_X+6, "(1) Error Summary");
 			cprint(POP_Y+4, POP_X+6, "(2) Individual Errors");
@@ -307,12 +308,12 @@ void get_config()
 					break;
 				}
 			}
-			popclear();
+			popclear(POP_SAVE_BUFFER_1);
 			break;
 		case 5:
     			/* CPU Mode */
     	reprint_screen = 1;
-			popclear();
+			popclear(POP_SAVE_BUFFER_1);
 			cprint(POP_Y+1, POP_X+2, "CPU Selection Mode:");
 			cprint(POP_Y+3, POP_X+6, "(1) Parallel (All)");
 			cprint(POP_Y+4, POP_X+6, "(2) Round Robin (RRb)");
@@ -326,25 +327,25 @@ void get_config()
 					if (cpu_mode != CPM_ALL) bail++;
 					cpu_mode = CPM_ALL;
 					sflag++;
-					popdown();
+					popdown(POP_SAVE_BUFFER_1);
 					cprint(9,34,"All");
-					popup();
+					popup(POP_SAVE_BUFFER_1);
 					break;
 				case 3:
 					if (cpu_mode != CPM_RROBIN) bail++;
 					cpu_mode = CPM_RROBIN;
 					sflag++;
-					popdown();
+					popdown(POP_SAVE_BUFFER_1);
 					cprint(9,34,"RRb");
-					popup();
+					popup(POP_SAVE_BUFFER_1);
 					break;
 				case 4:
 					if (cpu_mode != CPM_SEQ) bail++;
 					cpu_mode = CPM_SEQ;
 					sflag++;
-					popdown();
+					popdown(POP_SAVE_BUFFER_1);
 					cprint(9,34,"Seq");
-					popup();
+					popup(POP_SAVE_BUFFER_1);
 					break;
 				case 11:
 				case 57:
@@ -353,7 +354,7 @@ void get_config()
 					break;
 				}
 			}
-			popclear();
+			popclear(POP_SAVE_BUFFER_1);
 			break;
 		case 6:
 			reprint_screen = 1;
@@ -361,15 +362,15 @@ void get_config()
 			break;
 		case 7:
 			/* Display DMI Memory Info */
-			pop2up();
+			popup(POP_SAVE_BUFFER_2);
       print_dmi_info();
-			pop2down();			
+			popdown(POP_SAVE_BUFFER_2);
 			break;
 		case 8:
 			/* Display SPD Data */			
-			popdown();
+			popdown(POP_SAVE_BUFFER_1);
 			show_spd();
-			popup();
+			popup(POP_SAVE_BUFFER_1);
 			sflag++;
 			break;
 		case 11:
@@ -380,7 +381,7 @@ void get_config()
 			break;
 		}
 	}
-	popdown();
+	popdown(POP_SAVE_BUFFER_1);
 	if (prt) {
 		printpatn();
 	}
@@ -389,110 +390,63 @@ void get_config()
         }
 }
 
-void popup()
+void popup(int pop_x, int pop_y, int pop_h, int pop_w, char pop_save_buffer[2][pop_h][pop_w])
 {
 	int i, j;
 	char *pp;
 	
-	for (i=POP_Y; i<POP_Y + POP_H; i++) { 
-		for (j=POP_X; j<POP_X + POP_W; j++) { 
+	for (i=pop_y; i<pop_y + pop_h; i++) {
+		for (j=pop_x; j<pop_x + pop_w; j++) {
+			/* Point to the write position in the screen */
 			pp = (char *)(SCREEN_ADR + (i * 160) + (j * 2));
-                        save[0][i-POP_Y][j-POP_X] = *pp;  /* Save screen */
-                        set_scrn_buf(i, j, ' ');
-			*pp = ' ';		/* Clear */                        
+                        pop_save_buffer[0][i-pop_y][j-pop_x] = *pp;  /* Save screen */
+			*pp = ' ';		/* Clear screen */
+                        set_scrn_buf(i, j, ' '); /* Clear screen buffer */
 			pp++;
-                        save[1][i-POP_Y][j-POP_X] = *pp;
-			*pp = 0x07;		/* Change Background to black */
+                        pop_save_buffer[1][i-pop_y][j-pop_x] = *pp; /* Save screen background color */
+			*pp = 0x07;		/* Change screen Background to black */
 		}
 	}
-        tty_print_region(POP_Y, POP_X, POP_Y+POP_H, POP_X+POP_W);
+	/* print the screen buffer in the serial console */
+        tty_print_region(pop_y, pop_x, pop_y+pop_h, pop_x+pop_w);
 }
 
-void popdown()
+void popdown(int pop_x, int pop_y, int pop_h, int pop_w, char pop_save_buffer[2][pop_h][pop_w])
 {
 	int i, j;
 	char *pp;
 	
-	for (i=POP_Y; i<POP_Y + POP_H; i++) { 
-		for (j=POP_X; j<POP_X + POP_W; j++) { 
+	for (i=pop_y; i<pop_y + pop_h; i++) {
+		for (j=pop_x; j<pop_x + pop_w; j++) {
+			/* Point to the write position in the screen */
 			pp = (char *)(SCREEN_ADR + (i * 160) + (j * 2));
-			*pp = save[0][i-POP_Y][j-POP_X]; /* Restore screen */
-                        set_scrn_buf(i, j, save[0][i-POP_Y][j-POP_X]);
+			*pp = pop_save_buffer[0][i-pop_y][j-pop_x]; /* Restore screen */
+                        set_scrn_buf(i, j, pop_save_buffer[0][i-pop_y][j-pop_x]); /* Restore the screen buffer*/
 			pp++;
-			*pp = save[1][i-POP_Y][j-POP_X]; /* Restore color */
+			*pp = pop_save_buffer[1][i-pop_y][j-pop_x]; /* Restore screen color */
 		}
 	}
-        tty_print_region(POP_Y, POP_X, POP_Y+POP_H, POP_X+POP_W);
+	/* print the screen buffer in the serial console */
+        tty_print_region(pop_y, pop_x, pop_y+pop_h, pop_x+pop_w);
 }
 
-void popclear()
-{
-	int i, j;
-	char *pp;
-	
-	for (i=POP_Y; i<POP_Y + POP_H; i++) { 
-		for (j=POP_X; j<POP_X + POP_W; j++) { 
-			pp = (char *)(SCREEN_ADR + (i * 160) + (j * 2));
-			*pp = ' ';		/* Clear popup */
-                        set_scrn_buf(i, j, ' ');
-			pp++;
-		}
-	}
-        tty_print_region(POP_Y, POP_X, POP_Y+POP_H, POP_X+POP_W);
-}
-
-void pop2up()
+void popclear(int pop_x, int pop_y, int pop_h, int pop_w, char pop_save_buffer[2][pop_h][pop_w])
 {
 	int i, j;
 	char *pp;
 
-	for (i=POP2_Y; i<POP2_Y + POP2_H; i++) { 
-		for (j=POP2_X; j<POP2_X + POP2_W; j++) { 
+	for (i=pop_y; i<pop_y + pop_h; i++) {
+		for (j=pop_x; j<pop_x + pop_w; j++) {
+			/* Point to the write position in the screen */
 			pp = (char *)(SCREEN_ADR + (i * 160) + (j * 2));
-			save2[0][i-POP2_Y][j-POP2_X] = *pp;  /* Save screen */
-			set_scrn_buf(i, j, ' ');
-			*pp = ' ';		/* Clear */
-			pp++;
-			save2[1][i-POP2_Y][j-POP2_X] = *pp;
-			*pp = 0x07;		/* Change Background to black */
-		}
-	}
-        tty_print_region(POP2_Y, POP2_X, POP2_Y+POP2_H, POP2_X+POP2_W);
-}
-
-void pop2down()
-{
-	int i, j;
-	char *pp;
-
-	for (i=POP2_Y; i<POP2_Y + POP2_H; i++) { 
-		for (j=POP2_X; j<POP2_X + POP2_W; j++) { 
-			pp = (char *)(SCREEN_ADR + (i * 160) + (j * 2));
-			*pp = save2[0][i-POP2_Y][j-POP2_X]; /* Restore screen */
-			set_scrn_buf(i, j, save2[0][i-POP2_Y][j-POP2_X]);
-			pp++;
-			*pp = save2[1][i-POP2_Y][j-POP2_X]; /* Restore color */
-		}
-	}
-        tty_print_region(POP2_Y, POP2_X, POP2_Y+POP2_H, POP2_X+POP2_W);
-}
-
-void pop2clear()
-{
-	int i, j;
-	char *pp;
-
-	for (i=POP2_Y; i<POP2_Y + POP2_H; i++) { 
-		for (j=POP2_X; j<POP2_X + POP2_W; j++) { 
-			pp = (char *)(SCREEN_ADR + (i * 160) + (j * 2));
-			*pp = ' ';		/* Clear popup */
-			set_scrn_buf(i, j, ' ');
+			*pp = ' ';		/* Clear screen */
+                        set_scrn_buf(i, j, ' '); /* Clear screen buffer */
 			pp++;
 		}
 	}
-        tty_print_region(POP2_Y, POP2_X, POP2_Y+POP2_H, POP2_X+POP2_W);
+	/* print the screen buffer in the serial console */
+        tty_print_region(pop_y, pop_x, pop_y+pop_h, pop_x+pop_w);
 }
-
 
 void adj_mem(void)
 {

dmi.c
@@ -300,7 +300,7 @@ void print_dmi_info(void){
 	}
 
 	for(page=1; page <= 1 + (mem_devs_count-1)/8; page++){
-		pop2clear();
+		popdown(POP_SAVE_BUFFER_2);
 		cprint(POP2_Y+1, POP2_X+2, "DMI Memory Device Info  (page ");
 		itoa(string,page);
 		cprint(POP2_Y+1, POP2_X+32, string);

screen_buffer.c
@@ -111,6 +111,7 @@ void tty_print_screen(void)
         ttyprint(i,0, padding);
 #endif /* SCRN_DEBUG */
 
+//    clear_screen_buf();
     tty_print_region(0, 0, SCREEN_Y, SCREEN_X);
 }
 

spd.c
@@ -388,14 +388,14 @@ void show_spd(void)
     int index;
     int i, j;
     int flag = 0;
-    pop2up();
+    popup(POP_SAVE_BUFFER_2);
     wait_keyup();
     index = find_smb_controller();
     if (index == -1) {
 	cprint(POP2_Y, POP2_X+1, "SMBus Controller not known");
 	while (!get_key());
 	wait_keyup();
-	pop2down();
+	popdown(POP_SAVE_BUFFER_2);
 	return;
     }
     else cprint(POP2_Y, POP2_X+1, "SPD Data: Slot");    
@@ -413,7 +413,7 @@ void show_spd(void)
 	    wait_keyup();
 	}
     }
-    pop2down();
+    popdown(POP_SAVE_BUFFER_2);
 }
 
 int get_ddr3_module_size(int sdram_capacity, int prim_bus_width, int sdram_width, int ranks)

test.h
@@ -148,12 +148,13 @@ void check_input(void);
 void footer(void);
 void scroll(void);
 void clear_scroll(void);
-void popup(void);
-void popdown(void);
-void popclear(void);
-void pop2up(void);
-void pop2down(void);
-void pop2clear(void);
+extern char pop_save_buffer_1[2][POP_H][POP_W];
+extern char pop_save_buffer_2[2][POP2_H][POP2_W];
+#define POP_SAVE_BUFFER_1 POP_X, POP_Y, POP_H, POP_W, pop_save_buffer_1
+#define POP_SAVE_BUFFER_2 POP_X, POP_Y, POP_H, POP_W, pop_save_buffer_2
+void popup(int pop_x, int pop_y, int pop_h, int pop_w, char pop_save_buffer[2][pop_h][pop_w]);
+void popdown(int pop_x, int pop_y, int pop_h, int pop_w, char pop_save_buffer[2][pop_h][pop_w]);
+void popclear(int pop_x, int pop_y, int pop_h, int pop_w, char pop_save_buffer[2][pop_h][pop_w]);
 void get_config(void);
 void get_menu(void);
 void get_printmode(void);

commit 9bc6497f6e6b358878232c764221c9bcdfb4580e

fix input broken by commit f0560a599053a2f1e9273ffd569c9f868ffa1f86

lib.c
@@ -603,30 +603,23 @@ void set_cache(int val)
 int get_key() {
 	int c;
 
-/*	c = inb(0x64);
-	itoa(test, c);
-	serial_echo_print("aa");
-	serial_echo_print(test);
-	serial_echo_print("aa");
-
-	if ((c & 1) == 0) {
-		serial_echo_print("bbbbb");*/
-//		if (serial_cons) {
-			int comstat;
-			comstat = serial_echo_inb(UART_LSR);
-			if (comstat & UART_LSR_DR) {
-				c = serial_echo_inb(UART_RX);
-				/* Pressing '.' has same effect as 'c'
-				   on a keyboard.
-				   Oct 056   Dec 46   Hex 2E   Ascii .
-				*/
-				return (ascii_to_keycode(c));
-			}
-//		}
-		return(0);
-/*	}
-	c = inb(0x60);
-	return((c));*/
+	c = inb(0x64);
+	if (c & 0x01) {
+		c = inb(0x60);
+		return((c));
+	} else if (serial_cons) {
+		int comstat;
+		comstat = serial_echo_inb(UART_LSR);
+		if (comstat & UART_LSR_DR) {
+			c = serial_echo_inb(UART_RX);
+			/* Pressing '.' has same effect as 'c'
+			   on a keyboard.
+			   Oct 056   Dec 46   Hex 2E   Ascii .
+			*/
+			return (ascii_to_keycode(c));
+		}
+	}
+	return(0);
 }
 
 void check_input(void)